cca14a3292130340836136af7820b4eaa921a26a
Renata Muylaert <renatamuy@gmail.com>
2020-04-02 17:21:52 +1300

    run in R 3.6.3
    forced installation of ggsn and ggrepel

--- script_mapas.R
+++ script_mapas.R
@@ -1,14 +1,23 @@
+# Mapas no R 
+# Update 2020 run in R 3.6.3
+# Renata Muylaert
+
+# Para versões novas do R
+
+https://cran.r-project.org/bin/windows/Rtools/index.html
+
 #Instale estes pacotes direto dos GitHubs dos autores
-devtools::install_github("slowkow/ggrepel")
-devtools::install_github("oswaldosantos/ggsn")
+
+devtools::install_github("slowkow/ggrepel", force = TRUE)
+devtools::install_github("oswaldosantos/ggsn", force = TRUE)
 
 # Carregue os pacotes necessarios
 library(ggplot2)
 library(ggmap)
-library(ggsn)
 library(maps)
 library(mapdata)
 library(ggrepel)
+library(ggsn)
 
 #######################
 
@@ -20,6 +29,8 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 #Metodo 2: escreva o caminho completo da pasta para onde baixou os arquivos do GitHub
 #setwd("CAMINHO PARA A PASTA QUE VOCE BAIXOU NO GITHUB E DESCOMPRIMIU//")
 
+# Cheque o diretório de trabalho
+getwd()
 #######################
 
 #Importe o arquivo com os pontos do GPS em graus decimais e transforme-o em um objeto de R
@@ -38,10 +49,6 @@ g <- ggplot() + geom_polygon(data = area,
                              aes(x=long, y = lat, group = group),
                              fill = "lightgrey", color = "lightgrey") + #Note que voce pode mudar as cores do fundo e da borda
   coord_fixed(1.1) + #Use isto para o mapa ficar proporcional
-  geom_polygon(data = area, 
-               aes(x = long, y = lat, group = group), 
-               color = "white", fill = NA, size = 0.04) + #Aqui voce pode escolher a cor das bordas e a espessura delas
-  
   geom_point(data = pontos, aes(x = long, y = lat), 
              color = "purple", #Escolha a cor dos pontos
              size = 2, #Tamanho dos pontos
@@ -60,7 +67,7 @@ g <- ggplot() + geom_polygon(data = area,
         axis.title.x = element_text(size = 12, angle=0),
         axis.title.y = element_text(size = 12, angle=90))
 
-#Vizualize o mapa
+#Vizualize o mapa básico
 plot(g)
 
 #######################
@@ -73,6 +80,7 @@ g +
   ggsn::scalebar(area, dist = 100,location = "bottomright", transform = TRUE, #Adicione uma barra de escala
                  dist_unit = "km", st.dist = 0.03, st.size = 2, model = 'WGS84') +
   ggsn::north(area, scale = .1) #Adicione uma seta com o norte
+
 dev.off()
 
 #######################
